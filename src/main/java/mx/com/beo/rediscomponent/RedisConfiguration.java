a76767e866524cc15da9408ad95be400089df963
iangap <igaspar@novasolutionayatems.com>
2018-01-18 10:17:04 -0600

    Se agrego password a la configuracion de la conexion a Redis.

--- src/main/java/mx/com/beo/rediscomponent/RedisConfiguration.java
+++ src/main/java/mx/com/beo/rediscomponent/RedisConfiguration.java
@@ -3,26 +3,30 @@ package mx.com.beo.rediscomponent;
 
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.Protocol;
 
 public class RedisConfiguration {
 
     private String host;
     private Integer port;
+    private String password;
 
 
     public RedisConfiguration()
     {
         if(System.getenv("HOST_REDIS") != null && System.getenv("PUERTO_REDIS") != null)
         {
-            this.host= System.getenv("HOST_REDIS");
-            this.port= Integer.valueOf(System.getenv("PUERTO_REDIS"));
+            this.host = System.getenv("HOST_REDIS");
+            this.port = Integer.valueOf(System.getenv("PUERTO_REDIS"));
+            this.password = System.getenv("PASSWORD_REDIS");
         }
         else
         {
             host="localhost";
             port= 6379;
+            password="";
         }
-        jedisPool= new JedisPool(jedisPoolConfig,host,port);
+        jedisPool= new JedisPool(jedisPoolConfig,host,port,Protocol.DEFAULT_TIMEOUT,password);
     }
 
     public final JedisPoolConfig jedisPoolConfig = buildPoolConfig();
